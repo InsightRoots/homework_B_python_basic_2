c660e8405916a7c00bd6be4e5496cd897fa0aeaa
InsightRoots <insight.roots.inc@gmail.com>
2022-01-26 22:43:55 +0900

    B-2 改行なんて嫌いだ、だいっきらいだ！！！！！！！！

--- kuku_2.py
+++ kuku_2.py
@@ -1,15 +1,38 @@
+# a = [1 ,2 ,3]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for A in a:
+#     for B in b:
+#         print(f"Aの中身{A}, Bの中身{B}",end=' ')
+
 # dan = int(input("なんのだん？"))
 # kake = int(input("なにかける？"))
-# print(dan,dan*2,dan*3)
-# print(dan*kake)
+#
+# kake = [1,2,3,4]
+# dan = [1]
+# for k in kake:
+#     for d in dan:
+#         print(d*k,d*k*2,)
 
-dan = [1,2]
-kakeru = [1,2,3,4,5,6,7,8,9]
-for d in dan:
-    for k in kakeru:
-        print(d * k,end=' ')
+n = int(input("行数を入力してください: "))
+m = int(input("列数を入力してください: "))
+for g in range(1, n+1):
+    for j in range(1, m+1) :
+        print(g * j, end=" ")
+    print()
 
-#いったん飛ばす！！！！！！！！もういややー！
+# 出来たー！！！！！！！！！！！！！！！！
+
+# dan = [1,2]
+# kakeru = [1,2,3,4]
+# for d in dan:
+#     for k in kakeru:
+#         print(d             *k,end=' ')
+#
+# for i in range(5):
+#     for y in range(3):
+#         print(i * y,end=" ")
+
+#もういややー！
 #
 # multi_dan = [1]
 # multi_kake = [1,2,3,4,5,6,7,8,9]
@@ -28,6 +51,7 @@ for d in dan:
 # for A in a:
 #     for B in b:
 #         print(f"Aの中身{A}, Bの中身{B}",end=' ')
+
 #
 # a = [n for n in range(5)]
 # print(a)
